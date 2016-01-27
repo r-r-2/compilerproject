6c32df1db23656242d358b076aafba4f2b3d20f3
suraj94mec <suraj.94.mec@gmail.com>
2016-01-27 21:04:35 +0530

    Integratedwith sharejs

--- server/nodejs/gccs.js
+++ server/nodejs/gccs.js
@@ -1,53 +1,16 @@
 var fs = require("fs");
 var bodyParser = require('body-parser')
 var express=require("express");
-var ShareJS = require('share').server;
-var shareCodeMirror = require('share-codemirror');
+
 var mysql = require("mysql");
-var http = require('http');
-var Duplex = require('stream').Duplex;
-var browserChannel = require('browserchannel').server;
-var livedb = require('livedb');
-var sharejs = require('share');
-var shareCodeMirror = require('..');
-var backend = livedb.client(livedb.memory());
-var share = sharejs.server.createClient({backend: backend});
+
 var app=express();
 
-app.use(express.static(__dirname));
-app.use(express.static(shareCodeMirror.scriptsDir));
-app.use(express.static(__dirname + '/../node_modules/codemirror/lib'));
-app.use(express.static(sharejs.scriptsDir));
-app.use(browserChannel(function (client) {
-  var stream = new Duplex({objectMode: true});
-  stream._write = function (chunk, encoding, callback) {
-    if (client.state !== 'closed') {
-      client.send(chunk);
-    }
-    callback();
-  };
-  stream._read = function () {
-  };
-  stream.headers = client.headers;
-  stream.remoteAddress = stream.address;
-  client.on('message', function (data) {
-    stream.push(data);
-  });
-  stream.on('error', function (msg) {
-    client.stop();
-  });
-  client.on('close', function (reason) {
-    stream.emit('close');
-    stream.emit('end');
-    stream.end();
-  });
-  return share.listen(stream);
-}));
 
 
 var con = mysql.createConnection({
   host: "localhost",
-  user: "root",
+  user: "root"d,
   password: "root",
     database: "compilerproject",
   port: '/var/run/mysqld/mysqld.sock'    
@@ -109,9 +72,9 @@ app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
    extended: true
  })); 
 app.use(express.static(__dirname + '/public'));
-app.use(express.static(shareCodeMirror.scriptsDir));
+
 //app.use(bodyParser.json()); 
-ShareJS.attach(app, ShareJSOpts);
+
 console.log("Server starting up");
 
 //app.get('/', function(req,res) {
